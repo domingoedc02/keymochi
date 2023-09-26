59ae5c11bfaf38ebd888070776ccc08dd9bba09f
John <domingoedc02@gmail.com>
2023-09-26 14:34:29 +0800

    update

--- public/js/home.js
+++ public/js/home.js
@@ -1,16 +1,16 @@
-const element = document.getElementById("product");
+// const element = document.getElementById("product");
 
-element.scrollIntoView();
-element.scrollIntoView(false);
-element.scrollIntoView({ block: "end" });
-element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+// element.scrollIntoView();
+// element.scrollIntoView(false);
+// element.scrollIntoView({ block: "end" });
+// element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
